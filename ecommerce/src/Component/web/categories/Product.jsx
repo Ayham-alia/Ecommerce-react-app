dcb645fec04dd47d98fd1d3d60818da1f2234e92
Ayham alia <ayhamalia1234@gmail.com>
2024-03-18 14:53:49 +0200

    first commit
    first commit

---
+++ ecommerce/src/Component/web/categories/Product.jsx
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { Container, Row, Col, Image, Button, Form } from 'react-bootstrap';
+import { FaStar } from 'react-icons/fa';
+import Loader from '../../loader/Loader';
+import ReactImageMagnify from 'react-image-magnify';
+import { CartContext } from '../cart/Cart';
+
+
+function Product() {
+    let { id } = useParams()
+
+    const getProductDetails = async () => {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products/${id}`)
+        return data.product
+    }
+    const { data, isLoading } = useQuery('Product_data', getProductDetails)
+    const {addToCartContext}=useContext(CartContext)
+    const addToCart=async(id)=>{
+      await  addToCartContext(id)
+   }
+    return (
+        <>
+            {
+                isLoading ? (<Loader />) : (
+                    <Container>
+                        <Row className='mt-3 mb-4'>
+                            <Col md={6}>
+                                {/* Main product image */}
+                                <ReactImageMagnify {...{
+                                    smallImage: {
+                                        alt: 'Wristwatch by Ted Baker London',
+                                        isFluidWidth: true,
+                                        src: data.mainImage.secure_url
+                                    },
+                                    largeImage: {
+                                        src: data.mainImage.secure_url,
+                                        width: 1200,
+                                        height: 1800
+                                    }
+                                }} />
+                            </Col>
+                            <Col md={6}>
+                                {/* Product details */}
+                                <h2>{data.name}</h2>
+                                <p className='mt-4 w-75'>  <strong>Description :</strong> {data.description}</p>
+                                <p><strong>Price :</strong>  $ {data.price}</p>
+                                <Button variant="primary" onClick={()=>addToCart(data._id)}>Add to Cart</Button>
+                                <Row className='mt-4'>
+                                    {data.subImages.map((img) =>
+                                        <Col xs={6} md={3}>
+                                            <Image src={img.secure_url} thumbnail />
+                                        </Col>
+                                    )}
+                                    {/* Add more sub-images as needed */}
+                                </Row>
+
+                            </Col>
+                        </Row>
+                        {/* Sub-images (thumbnails) */}
+
+                        <Row>
+                        </Row>
+                        {/* Review section */}
+                        <Row>
+
+                            {/* Review form */}
+                            <Col>
+                                <Form >
+                                    <Form.Group controlId="formAuthormb-2">
+                                        <Form.Label>Your Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter your name" />
+                                    </Form.Group>
+                                    <Form.Group controlId="formRating">
+                                        <Form.Label>Rating</Form.Label>
+                                        <Form.Control as="select">
+                                            <option>5</option>
+                                            <option>4</option>
+                                            <option>3</option>
+                                            <option>2</option>
+                                            <option>1</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group controlId="formComment">
+                                        <Form.Label>Your Review</Form.Label>
+                                        <Form.Control as="textarea" rows={3} placeholder="Write your review" />
+                                    </Form.Group>
+                                    <Button type="submit" className='mt-2 btn mb-2'>
+                                        Submit Review
+                                    </Button>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Container>
+
+                )
+            }
+
+
+        </>
+    )
+}
+
+export default Product
